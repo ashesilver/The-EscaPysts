0885ec78d04d779618a5a3f7334efecd02024c2c
thib ters <43893686+ashesilver@users.noreply.github.com>
2019-02-22 15:18:21 +0100

    Update gridElements.py

--- gridElements.py
+++ gridElements.py
@@ -2,18 +2,19 @@
 # -*- coding:utf-8 -*-
 
 var = {
-	'lvl0':[{ "gate":[{					#### gates are doors to go from teem to another in same level
+	'lvl0':[{ "gate":{					#### gates are doors to go from teem to another in same level
 				'image':'./gate.jpg', 		   #### image Adresse 
-				'positions':[{},{},{}],
-				'size':[{"L"},{"H"}],
+				'position':[{},{},{}],
+				'size':["L","H"],
 				'destination':[{},{},{}]  	  #### which room to go 
-				}]
+				}
 			},{
-			  "hideout":[{
+			  "hideout":{
 			  	'image':'Image Adresse',
-			  	'positions':[{},{},{}],
-			  	'size':[{'L'},{'H'}]
-				}]
+			  	'position':[{},{},{}],
+			  	'size':['L','H'],
+			  	'destination':[{},{},{}]
+				}
 			}],
 	'lvl1':[{},{},{}],
 	'lvl2':[{}]
