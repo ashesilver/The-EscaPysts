c2949f5b53e746cad8763b945ecbc54f5de0af39
thib ters <43893686+ashesilver@users.noreply.github.com>
2019-02-25 10:13:24 +0100

    Update externalCore.py

--- externalClasses/externalCore.py
+++ externalClasses/externalCore.py
@@ -1,3 +1,102 @@
 #!/bin/usr/python3
 #-*-coding:utf8;-*-
 
+class Core():
+	"""classe prinicpale gérant toutes les autres"""
+
+	graphicHandlerObject = Graphics(800,600)
+	clock = pygame.time.Clock()
+	
+	def __init__(self, FPS_limit=240):
+		self.fpsLimit = FPS_limit
+		self.quit = False
+
+	"""
+	def playableInLoop(self):
+		run = True
+		while run:
+			if type(self.keys) == bool:
+				self.quit = True
+			run = not(self.quit)
+	"""
+			
+	def keyLock(self):
+		for x in self.keys:
+			if x in self.keysRegister:
+				if x != "U" and x !="D" and x !="L" and x !="R":
+					self.keys.remove(x)
+			else :
+				if x != "U" and x !="D" and x !="L" and x !="R":
+					self.keysRegister.append(x)
+				
+
+
+	def run(self):
+
+		run = True
+		static = True #(menu)
+
+		options = False
+		play = False
+
+		####### game loop
+		while run:
+			Core.clock.tick(self.fpsLimit) #defines clock's max speed by (1/FPS_limit) ms per frame
+			self.keys = self.graphicHandlerObject.getKeys()
+
+			#temporaryKeyLock = copy.deepcopy(self.keys)
+			try :
+				temporaryKeyLock = self.keys[:]
+				self.keyLock()
+			
+			except :
+				####### game-ender
+				if type(self.keys) == bool:
+					run = not(self.keys)
+
+			else :
+				####### level trigger
+				if ("ENTER" in self.keys or "Enter" in self.keys) or play:
+					if not play:
+						self.levelHandlerObject = Level(self.graphicHandlerObject)
+						self.playerHandlerObject = Player(self.graphicHandlerObject,self.levelHandlerObject,self.keys)
+						self.ennemiesHandlerObject = Ennemies(self.graphicHandlerObject,self.levelHandlerObject,self.playerHandlerObject)
+						play = True
+						static = False
+						# self.levelHandlerObject.test()
+					self.playerHandlerObject.keys = self.keys[:]
+					self.ennemiesHandlerObject.walk()
+					self.playerHandlerObject.move_player()
+					
+
+						
+
+					"""
+					x = self.memoryPath()
+					if not(bool(x)):
+						self.levelHandlerObject = Level(int(x))
+					else :
+						self.levelHandlerObject = Level()
+					"""
+
+
+					####### in-level actions :
+
+
+				####### options menu trigger
+				if "esc" in self.keys or options:
+					if "esc" in self.keys:
+						options = not(options)
+					static = False
+				
+				####### main menu
+				if static:
+					pass
+
+
+				####### endLoop actions
+				self.keysRegister = temporaryKeyLock[:]
+
+				####### tests zone
+
+				#print(self.keys)
