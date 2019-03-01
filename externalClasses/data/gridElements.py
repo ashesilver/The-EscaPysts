416d78a1009e2a37f2d4e45158c44000a7ce6812
Marwan Mashra <47461193+MarwanMashra@users.noreply.github.com>
2019-03-01 14:12:44 +0100

    Update gridElements.py

--- externalClasses/data/gridElements.py
+++ externalClasses/data/gridElements.py
@@ -6,25 +6,29 @@ var = {
 				'image':'images/gate.jpg', 		   #### image Adresse 
 				'position':[],
 				'size':["L","H"],
-				'destination':[]  	  #### which room to go 
+				'destination':[],         #### which room to go
+				'hideable':False 	   
 				},
 			"flowerPot":{
 			  	'image':'images/flowerPot.jpg',
 			  	'position':[{'x':35,'y':10}],
 			  	'size':[1,1],
-			  	'destination':[{}]
+			  	'destination':[{}],
+			  	'hideable':True
 				},
 			"path":{
 				'image':'images/ground.jpg',
 				'position' : [],
 				'size' : ['l','h'],
-				'destination' : []
+				'destination' : [],
+				'hideable':False
 				},
 			"wall":{
-				'image':'images/wall.jpg',
-				'position' : [],
-				'size' : ['l','h'],
-				'destination' : []
+				'image':'images/wall.png',
+				'position' : [{'x':20,'y':30},{'x':45,'y':65},{'x':10,'y':45}],
+				'size' : [1,1],
+				'destination' : [],
+				'hideable':False
 				}
 			},
 	'lvl1':[{},{},{}],
