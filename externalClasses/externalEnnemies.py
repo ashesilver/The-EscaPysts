f79e5a8e8f53686d34e0763fb49345e58de57efc
hizeer <47298056+hizeer@users.noreply.github.com>
2019-03-01 14:48:13 +0100

    vision de l'ennemis
    lalallalalaa

--- externalClasses/externalEnnemies.py
+++ externalClasses/externalEnnemies.py
@@ -11,36 +11,55 @@ class Ennemies():
 		self.position = [20,15]
 		self.positionPrec= [0,0]
 		self.stun = False
-		self.triggered = False
 		self.graphicHandlerObject.drawCircle(self.position[0],self.position[1])
-		self.direction="up"
-
+		self.direction = "up"
+		self.vision = [[0][0]]
 		self.size = [1,1] #ah j'en ai besoin pour graphique tu touche pas hein !
 		self.imageAdress = "./images/ennemy.jpg"
-
 		self.walkTick = 0
+		self.vision = [[],[]]
 
 	def walk(self):
 		self.walkTick += 1
-		if not self.stun and not self.triggered and not self.walkTick%4: #walk activates each %x frames
+		if not self.stun and not self.walkTick%4: #walk activates each %x frames
 			self.positionPrec = self.position[:]
-			if self.position[0]==20 and self.position[1]!=6:
+			if self.position[0]==20 and self.position[1]!=6: #up
+				self.direction = "up"  
 				self.position[1]-=1
-			elif self.position[0]!=32 and self.position[1]==6:
+				self.vision = [[],[]]
+				for i in range(5):
+					self.vision[1].append(self.position[1]-i)
+
+			elif self.position[0]!=32 and self.position[1]==6: #right
+				self.direction = "right" 
 				self.position[0]+=1
-			elif self.position[0]==32 and self.position[1]!=20:
+				self.vision = [[],[]]
+				for i in range(5):
+					self.vision[0].append(self.position[0]+i)
+
+			elif self.position[0]==32 and self.position[1]!=20: #down
+				self.direction = "down" 
 				self.position[1]+=1
-			elif self.position[1]==20 and self.position[0]!=15:
+				self.vision = [[],[]]
+				for i in range(5):
+					self.vision[1].append(self.position[1]+i)
+
+			elif self.position[1]==20 and self.position[0]!=15: #left
+				self.direction = "left" 
 				self.position[0]-=1
-			
+				self.vision = [[],[]]
+				for i in range(5):
+					self.vision[0].append(self.position[0]-i)
 
 	def followPlayer(self,other):
-		if not self.stun and self.triggered:
-			pass
+		if self.playerHandlerObject.position[0] in self.vision[0] or self.playerHandlerObject.position[1] in self.vision[1]:
+				self.graphicHandlerObject.killWindow()
+		#if not self.stun and not self.playerHandlerObject.hidden:
+		#	print("lalalalala")
+			
 
-		
 	def search(self,other):
-		if not self.stun and self.triggered : #and other.hidden
+		if not self.stun and self.playerHandlerObject.hidden: #and other.hidden
 			pass
 
 	def update(self):
