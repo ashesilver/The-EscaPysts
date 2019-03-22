db2bf062c943ec53f519a902f2f3358b2349b6fb
thib ters <43893686+ashesilver@users.noreply.github.com>
2019-03-22 17:10:35 +0100

    Update level_editor.py

--- level_editor.py
+++ level_editor.py
@@ -3,6 +3,8 @@
 
 from externalClasses.externalGraphics import *
 
+import os
+
 gr = Graphics()
 
 clock = pygame.time.Clock()
@@ -42,13 +44,31 @@ Wsave = {"gate":{					#### gates are doors to go from teem to another in same le
 				'hideable':False
 				}
 			}
-c1,c2,c3,c4,c5,c6 = 0,0,0,0,0,0
+common = [[]]
+
+c1=0
+
+keysRegister = []
+
+allEnnemies = []
+
+helper = "flèches :\n gauche -> passer en mode 'ennemis'\n droite -> passer en mode 'mur' (par défault)\n haut -> chager de direction pour les ennemis\n bas -> créer un nouvel ennemis (et enregistrer le précedent)\nEntrée (pav num et principal) :\n afficher la référence à copier-coller\néchap :\n affiche cette aide\nclics :\n gauche -> ajouter position\n droite -> enlever position\n\n"
 
 mode = "walls"
 print("new mode : walls")
 
 def onGridMousePos(x):
 	return [int(x[0]/800*60),int(x[1]/800*60)]
+
+def keyLock(keys,keysRegister):
+	for x in ["U","L","D","R","Enter","ENTER","esc","1","2","3","4"]:
+		if ( not x in keys) and x in keysRegister :
+			keysRegister.remove(x)
+		elif x in keysRegister:
+			keys.remove(x)
+		elif x in keys and not x in keysRegister:
+			keysRegister.append(x)
+	return (keys,keysRegister)
 
 gr.displayBackgroundUpdate("images/lvl0.png")
 
@@ -58,6 +78,7 @@ while not quit :
 	if type(keys)==bool :
 		pygame.quit() 
 		quit = True
+
 	else :
 		gr.displayBackgroundUpdate()
 
@@ -65,31 +86,70 @@ while not quit :
 
 		mousePos = onGridMousePos(gr.getMouse())[:]
 
+		x = keyLock(keys,keysRegister)
+		keys,keysRegister = x[0][:],x[1][:]
+
+		if "esc" in keys :
+			print(helper)
+
 		if "L" in keys :
 			print("new mode : ennemies")
 			mode = "ennemies"
 		elif "R" in keys :
 			print("new mode : walls")
 			mode = "walls"
-		if 'esc' in keys and mode == "ennemies":
-			common = [[]]
-			print("new ennemy")
-		elif "U" in keys and mode == 'ennemies':
-			common.append([])
-			print("new row / column")
-			c1 += 1
 
 		if mode == "walls" :
+
 			if gr.leftClick and not mousePos in Wsave["wall"]["position"]:
 				Wsave["wall"]["position"].append(mousePos)
+			
 			elif gr.rightClick and mousePos in Wsave["wall"]["position"]:
 				Wsave["wall"]["position"].remove(mousePos)
+			
 			for x in Wsave["wall"]["position"] :
 				gr.displaySquare(x)
-			if "enter" in keys or "ENTER" in keys :
+			
+			if ("enter" in keys) or ("ENTER" in keys):
+				if os.name == "posix" :
+					os.system("clear")
+				elif os.name == "nt" :
+					os.system("cls")
 				print(mode+ " " + str(Wsave))
 
+		elif mode == "ennemies" :
+
+			if 'D' in keys:
+				allEnnemies.append(common)
+				common = [[]]
+				c1 = 0
+				print("new ennemy pattern")
+			
+			elif "U" in keys:
+				common.append([])
+				print("new row / column")
+				c1 += 1
+			
+			if ( "enter" in keys ) or ("ENTER" in keys) :
+				if os.name == "posix" :
+					os.system("clear")
+				elif os.name == "nt" :
+					os.system("cls")
+				print(mode+ " " + str(allEnnemies))
+			
+			if gr.leftClick and not mousePos in common[c1]:
+				common[c1].append(mousePos)
+			elif gr.rightClick and mousePos in common[c1] :
+				common[c1].remove(mousePos)
+
+			for x in Wsave["wall"]["position"] :
+				gr.displaySquare(x,1)
+			for x in common:
+				for y in x :
+					if x != common[c1] :
+						gr.displaySquare(y,2)
+			for x in common[c1] :
+				gr.displaySquare(x)
+				
 		gr.generalDisplayUpdate()
 
-
-#ajout gauche, supr droite
